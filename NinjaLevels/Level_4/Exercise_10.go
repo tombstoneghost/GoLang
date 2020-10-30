c8e4542b7b9282e38cd6febe9493ccf86c3b91d0
tombstoneGhost <simardeepsngh1@gmail.com>
2020-10-31 00:36:28 +0530

    Ninja Level-4

---
+++ NinjaLevels/Level_4/Exercise_10.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	mapData := map[string][]string{
+		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+		"no_dr":           []string{"Being evil", "Ice Cream", "Sunsets"},
+	}
+
+	mapData["fleming_ian"] = []string{"steaks", "cigars", "espionage"}
+
+	delete(mapData, "no_dr")
+
+	for k, v := range mapData {
+		fmt.Println("Record: ", k)
+
+		for i, val := range v {
+			fmt.Printf("\t%v\t%v\n", i, val)
+		}
+	}
+}
