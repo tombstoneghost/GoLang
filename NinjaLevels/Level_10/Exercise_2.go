c8ded4b39ca47f4735c1ab509efa5e4a8531717c
tombstoneGhost <simardeepsngh1@gmail.com>
2020-11-05 16:39:17 +0530

    Ninja Level-10

---
+++ NinjaLevels/Level_10/Exercise_2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Part-1
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+
+	// Part-2
+	cr := make(chan int)
+
+	go func() {
+		cr <- 42
+	}()
+	fmt.Println(<-cr)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cr\t%T\n", cr)
+}
